5390989d52b8ddc8033c3623ec122e434830feca
MADUPAL <kevin488@naver.com>
2024-03-07 12:01:47 +0900

    middleware and routes
    login, logout, my info routes

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -7,6 +7,7 @@ import jwt from "jsonwebtoken"
 connect()
 
 export async function POST(request: NextRequest) {
+  try{
     const reqBody = await request.json()
     const {email, password} = reqBody;
     console.log(reqBody);
@@ -16,11 +17,13 @@ export async function POST(request: NextRequest) {
     if(!user) {
       return NextResponse.json({error:"User does not exist"}, {status: 400})
     }
+    console.log("user exists");
     //check if password is correct
     const validPassword = await bcryptjs.compare(password, user.password);
     if(!validPassword) {
       return NextResponse.json({error: "Invalid password"}, {status:400})
     }
+    console.log(user);
     //create token data
     const tokenData = {
       id: user.id,
@@ -38,4 +41,8 @@ export async function POST(request: NextRequest) {
     response.cookies.set("token", token, {httpOnly: true})
 
     return response;
+    
+  } catch (error: any) {
+      return NextResponse.json({error: error.message}, {status: 500})
+  }
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,52 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function ProfilePage() {
+  const router = useRouter();
+
+  const [data, setData] = useState("nothing");
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout Successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div>
       <h1>Profile</h1>
+      <hr />
+      <h2>
+        {data === "nothing" ? (
+          "Nothing here"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <button
+        className="bg-blue-700 mt-4 py-2 px-4 rounded"
+        onClick={getUserDetails}
+      >
+        get details
+      </button>
+      <button className="mt-4 py-2 px-4 rounded" onClick={logout}>
+        Logout
+      </button>
     </div>
   );
 }
